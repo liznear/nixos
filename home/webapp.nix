3aa34e4b67919dec24213c58c1393eafc6a6b87b
Liiiz <liizznear@gmail.com>
2025-11-09 16:53:47 +0800

    Fix webapp icon

--- home/webapp.nix
+++ home/webapp.nix
@@ -2,13 +2,13 @@
 
 {
   xdg.desktopEntries = {
-    chatgpt = {
+    "vivaldi-chatgpt.com__-Default" = {
       name = "ChatGPT";
       genericName = "AI";
       exec = "vivaldi --app=https://chatgpt.com";
       terminal = false;
       categories = [ "Utility" ];
-      icon = "/home/nearsyh/.icons/chatgpt.png";
+      icon = "/home/nearsyh/.icons/chatgpt.svg";
     };
   };
 }
