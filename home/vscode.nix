9ed01379296c9283c6c9670987b31991bee8ee43
Liiiz <liizznear@gmail.com>
2025-11-12 20:54:58 +0800

    Update vscode config

--- home/vscode.nix
+++ home/vscode.nix
@@ -4,8 +4,24 @@
   programs.vscode = {
     enable = true;
     profiles.default = {
-      userSettings = {
+      userSettings = lib.mkForce {
         "chat.disableAIFeatures" = true;
+        "chat.editor.fontFamily" = "Maple Mono NF CN";
+        "chat.editor.fontSize" = 15.0;
+        "debug.console.fontFamily" = "Maple Mono NF CN";
+        "debug.console.fontSize" = 15.0;
+        "editor.fontFamily" = "Maple Mono NF CN";
+        "editor.fontSize" = 15.0;
+        "editor.inlayHints.fontFamily" = "Maple Mono NF CN";
+        "editor.inlineSuggest.fontFamily" = "Maple Mono NF CN";
+        "editor.minimap.sectionHeaderFontSize" = 11;
+        "markdown.preview.fontFamily" = "Fira Sans";
+        "markdown.preview.fontSize" = 15.0;
+        "scm.inputFontFamily" = "Maple Mono NF CN";
+        "scm.inputFontSize" = 15;
+        "screencastMode.fontSize" = 64.0;
+        "terminal.integrated.fontSize" = 15.0;
+        "workbench.colorTheme" = "Stylix";
       };
       extensions = with pkgs.vscode-extensions; [
         ms-toolsai.jupyter
@@ -13,6 +29,7 @@
         mkhl.direnv
         catppuccin.catppuccin-vsc
         catppuccin.catppuccin-vsc-icons
+        vscodevim.vim
       ];
     };
   };
