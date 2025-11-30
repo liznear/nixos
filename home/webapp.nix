5311c794009ef6016e2d184087d1f8c6eb9058ff
Liiiz <liizznear@gmail.com>
2025-11-30 10:05:48 +0800

    Switch to chrome

--- home/webapp.nix
+++ home/webapp.nix
@@ -2,18 +2,18 @@
 
 {
   xdg.desktopEntries = {
-    "vivaldi-chatgpt.com__-Default" = {
+    "google-chrome-stable-chatgpt.com__-Default" = {
       name = "ChatGPT";
       genericName = "AI";
-      exec = "vivaldi --app=https://chatgpt.com";
+      exec = "google-chrome-stable --app=https://chatgpt.com";
       terminal = false;
       categories = [ "Utility" ];
       icon = "/home/nearsyh/.icons/chatgpt.svg";
     };
-    "vivaldi-music.youtube.com__-Default" = {
+    "google-chrome-stable-music.youtube.com__-Default" = {
       name = "Youtube Music";
       genericName = "Music";
-      exec = "vivaldi --app=https://music.youtube.com";
+      exec = "google-chrome-stable --app=https://music.youtube.com";
       terminal = false;
       categories = [ "Audio" ];
       icon = "/home/nearsyh/.icons/youtube_music.svg";
