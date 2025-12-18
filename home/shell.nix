679f5ecc559be84c22fa2d381c2cc2288f2991a9
Liiiz <liizznear@gmail.com>
2025-12-18 21:13:14 +0800

    Fix errors

--- home/shell.nix
+++ home/shell.nix
@@ -2,18 +2,15 @@
 
 {
   imports = [
-    inputs.dankMaterialShell.homeModules.dankMaterialShell.default
-    inputs.dankMaterialShell.homeModules.dankMaterialShell.niri
+    inputs.dankMaterialShell.homeModules.default
+    inputs.dankMaterialShell.homeModules.niri
   ];
 
   programs.dankMaterialShell = {
     enable = true;
     enableSystemMonitoring = true;
-    enableClipboard = true;
-    enableBrightnessControl = true;
     enableDynamicTheming = true;
     enableAudioWavelength = true;
     enableCalendarEvents = true;
-    enableSystemSound = true;
   };
 }
